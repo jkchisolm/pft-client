328780e8de4348de6d06188a659256384aef6c0c
jkchisolm <jkchisolm22@gmail.com>
2024-04-18 00:30:30 -0700

    feat: scaffold basic homepage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,21 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        // eslint-disable-next-line
+        "cerulean": "#0081A7",
+        "verdigris": "#00AFB9",
+        "dark-bg": "#12130F",
+        "light-bg": "#FDFCDC",
+        "lightorange": "#FED9B7",
+        "bittersweet": "#F07167"
+      },
+      dropShadow: {
+        glow: [
+          "0 0px 20px rgba(255,255, 255, 0.35)",
+          "0 0px 65px rgba(255, 255,255, 0.2)"
+        ]
+      }
     },
   },
   plugins: [],
